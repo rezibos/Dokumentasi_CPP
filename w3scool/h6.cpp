e51f271c3d09f898f39ad9c079db00fb138cac18
rezibos <rezibos>
2024-11-07 15:11:40 +0700

    dua

---
+++ w3scool/h6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// OPERATORS
+
+// Contoh Operators Arithmetic
+// Tambah = x + y
+// Kurang = x -y
+// Kali = x * y
+// Bagi = x / y
+// Modulus = x % y // sisa bagi
+// Tambah-tambahkan = ++x
+// Kurang-kurang = --x
+
+
+int main()
+{
+    int sum1 = 100 + 50;        
+    int sum2 = sum1 + 250;      
+    int sum3 = sum2 + sum2; 
+}
