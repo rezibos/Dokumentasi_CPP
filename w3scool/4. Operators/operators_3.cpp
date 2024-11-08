278d40c97a48a880d5a952ab37098a945ff06404
rezibos <rezibos>
2024-11-08 20:25:00 +0700

    cpp

---
+++ w3scool/4. Operators/operators_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Operators
+
+// Logical
+
+// Anda juga dapat menguji nilai benar (1) atau salah (0) dengan operator logika
+// Contoh
+// Logical and
+// && = Mengembalikan true jika kedua pernyataan benar
+
+// Logical or
+// || = Mengembalikan true jika salah satu pernyataan benar
+
+// Logical not = untuk membalikkan hasil suatu kondisi
+// ! = Membalikkan hasil, mengembalikan false jika hasilnya benar
+
+
+int main()
+{
+    int x = 5;
+    int y = 3;
+    cout << (!(x > 3 && x < 10));
+}
