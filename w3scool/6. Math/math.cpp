6a6918dffacc28f12fa6674187007366d797ccdc
rezibos <rezibos>
2024-11-11 21:35:04 +0700

    cpp

---
+++ w3scool/6. Math/math.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Math
+
+// Contoh Math
+// Max : max(x, y) = digunakan untuk menemukan nilai x dan y tertinggi
+// Min : min(x,y) = digunakan untuk menemukan nilai x dan y terendah
+
+// Contoh Cmath
+// sqrt : (akar kuadrat)
+// round : (membulatkan angka)
+// log   : (logaritma natural)
+
+int main()
+{
+    // Contoh Max
+    cout << max(5, 10);
+
+    // Contoh Min
+    cout << min(5, 10);
+
+    // Penggunaan Cmath
+    cout << sqrt(64);
+    cout << round(2.6);
+    cout << log(2);
+}
