3be27eeafbf3a8dedc13ca2a3044c8cbf0c18cfa
rezibos <rezibos>
2024-11-21 12:35:51 +0700

    cpp

--- CodePolitan/h31_c.cpp
+++ CodePolitan/h31_c.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
+// Latihan Rekursif Fibonaci
+// Kekurangan Rekursif Kinerja Yang Lambat Saat Menghitung Angka Yang Banyak Dan Memakan Memori Yang Banyak 
+int fibonaci(int n);
+
 int main()
 {
-    
+    int anggka, hasil;
+
+    cout << "Masukan Anggka : ";
+    cin >> anggka;
+
+    hasil = fibonaci(anggka);
+
+    cout << "Nilainya Adalah : " << hasil << endl;
+}
+
+int fibonaci(int n){
+    if((n == 0) || (n == 1)){
+        return n;
+    } else {  // Ini yang akan menjadi eksponansial           
+        return fibonaci(n - 1) + fibonaci(n - 2);
+    }
 }
---
+++ CodePolitan/h32_c.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// Pointer 
+// Kegunaan Pointer : memudahkan memanace memory  atau untuk mengakses alamat yang kita mau pakai
+// Pointer itu adalah alamat yang bisa kita pakai untuk menarok fariabel 
+
+// Kita tidak bisa merubah nilai a dengan memanggil addressnya saja seperti ( &nama_fungsi ) gini aja, kita harus deklarasikan seperti ini int ( *nama_fungsi = &nama_fungsi ) baru kita tinggal panggil nama fungsinya aja
+
+// &nama_fungsi : buat menngetahui alamat atau address
+// *nama_fungsi : (ini buat alamat di deklarasikan) dan (bisa di panggil dengan memanggil nama_fungsi) dan (bisa merubah isi dari alamat itu dengan memanggil nama_fungsi)
+// nullptr : artinya kita mendeklarasikan sebuah pointer tapi isinya kosong atau tidak ada isi. ( adders yang masih bleng )
+// dereferencing : adalah menggambil data dari sebuah pointer
+
+// *nama_fungsi : buat ngambildata
+// namafungsi = &a : buat mengetahui alamat / address
+
+int main()
+{
+    // Ini int a mempunyai nilai atau address(alamat)
+    int a = 5;
+
+    // Pointer
+    int *ayam = nullptr;
+    ayam = &a;
+
+    cout << "Nilai A adalah : " << a << endl;
+    cout << "Alamat Dari Nilai A adalah : " << ayam << endl;
+
+    // Dereferencing
+    a = 10;
+    cout << "Mengambil Data Dari Nilai A : " << *ayam << endl;
+
+}
