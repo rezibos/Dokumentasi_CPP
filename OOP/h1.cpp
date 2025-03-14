427febbe5e35b66aae22c5f8b5559a5ab0518a79
rezibos <rezibos>
2025-03-14 14:06:52 +0700

    cpp

--- OOP/h1.cpp
+++ OOP/h1.cpp
@@ -3,10 +3,24 @@
 using namespace std;
 
 class Mahasiswa{
-    
+    public:
+        string nama;
+        string NIM;
+        string jurusan;
+        double IPK;
+
 };
 
 int main()
 {
-    Mahasiswa mahasiswa;
+    Mahasiswa data1;
+    data1.nama = "ucup";
+    data1.NIM = 28371823;
+    Mahasiswa data2;
+    data1.nama = "otong";
+
+    cout << "nama dari data1 : " << data1.nama << endl;
+    cout << "NIM dari data1 : " << data1.NIM << endl;
+    cout << "nama dari data2 : " << data2.nama << endl;
+
 }
---
+++ OOP/h2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// class tanpa constructor
+class Polos 
+{
+    public:
+        string nama;
+        int NIM;
+};
+
+// class dengan constructor : akan di panggil pertama kali ketika object di buat
+class Mahasiswa
+{
+    public:
+        string nama;
+        string NIM;
+        string jurusan;
+        double IPK;
+
+        // ini adalah constructor
+        // Mahasiswa(){
+            
+        // }
+
+        Mahasiswa(string InputNama, string InputNim, string InputJurusan, double InputIpk){
+            Mahasiswa::nama = InputNama;
+            Mahasiswa::NIM = InputNim;
+            Mahasiswa::jurusan = InputJurusan;
+            Mahasiswa::IPK = InputIpk;
+
+            cout << Mahasiswa::nama << endl;
+            cout << Mahasiswa::NIM << endl;
+            cout << Mahasiswa::jurusan << endl;
+            cout << Mahasiswa::IPK << endl;
+        }
+};
+
+
+int main()
+{
+    Mahasiswa mahasiswa1 = Mahasiswa("ucup", "238723", "teknik informatika", 4.6);
+    Mahasiswa mahasiswa2 = Mahasiswa("otong", "328790", "teknik perikanan", 3.6);
+
+    // cout << mahasiswa2.nama << endl;
+    // cout << mahasiswa2.NIM << endl;
+    // cout << mahasiswa2.jurusan << endl;
+    // cout << mahasiswa2.IPK << endl;
+
+    return 0;
+}
