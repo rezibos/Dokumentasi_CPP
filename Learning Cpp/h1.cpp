052ba4959e8777310fe284a446217f145c58b85e
Fachrezi Bachri <117134349+rezibos@users.noreply.github.com>
2025-04-19 20:36:38 +0700

    Delete Learning Cpp directory

--- Learning Cpp/h1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <limits>
-
-int main(){
-    std::cout << "abcd" << std::endl;
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cin.get();
-}
